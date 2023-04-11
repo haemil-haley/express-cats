a7c6d914a4e0ee03a3fe7a54f1df9e78e9ade18e
haemil <haemil.haley@gmail.com>
2023-04-11 21:38:55 +0900

    - add .gitignore - add hello world

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
-console.log("hello world!!");
+import * as express from "express";
+
+// const app: express.Application = express();
+const app: express.Express = express();
+const port = 8000
+
+app.get('/', (req: express.Request, res: express.Response) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost: ${port}`)
+})
